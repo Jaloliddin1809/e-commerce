f995645eb5ee79b68a919982f4546f83b9cc6d56
neo <neo.darkness.001@mail.ru>
2023-05-28 15:05:39 +0500

    telegram bot

--- src/main/java/uz/g4/ecommerce/repository/product/ProductRepository.java
+++ src/main/java/uz/g4/ecommerce/repository/product/ProductRepository.java
@@ -1,4 +1,17 @@
 package uz.g4.ecommerce.repository.product;
 
-public class ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uz.g4.ecommerce.domain.dto.response.BaseResponse;
+import uz.g4.ecommerce.domain.dto.response.ProductResponse;
+import uz.g4.ecommerce.domain.entity.product.ProductEntity;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
+//    Optional<ProductEntity> findById(UUID id);
+    List<ProductEntity> getProductEntitiesByCategory_Id(UUID categoryId);
 }
